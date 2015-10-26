595bf2f189807abc33028ecab615cd9bae08ae39
Dave Alden <dpa99c@gmail.com>
2015-10-26 08:52:25 +0000

    Remove unused properties and methods

--- src/ios/AppDelegate+notification.h
+++ src/ios/AppDelegate+notification.h
@@ -1,10 +1,4 @@
-//
-//  AppDelegate+notification.h
-//  pushtest
-//
-//  Created by Robert Easterday on 10/26/12.
-//
-//
+
 
 #import "AppDelegate.h"
 @interface AppDelegate (notification)
@@ -12,10 +6,6 @@
 - (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken;
 - (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error;
 - (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo;
-//- (void)applicationDidBecomeActive:(UIApplication *)application;
 - (void)application:(UIApplication *)application handleActionWithIdentifier:(NSString *)identifier forRemoteNotification:(NSDictionary *)userInfo completionHandler:(void(^)())completionHandler;
-//- (id) getCommandInstance:(NSString*)className;
-
-//@property (nonatomic, retain) NSDictionary  *launchNotification;
 
 @end
--- src/ios/AppDelegate+notification.m
+++ src/ios/AppDelegate+notification.m
@@ -1,57 +1,11 @@
-//
-//  AppDelegate+notification.m
-//  pushtest
-//
-//  Created by Robert Easterday on 10/26/12.
-//
-//
 
 #import "AppDelegate+notification.h"
 #import "DNNotificationController.h"
 #import <objc/runtime.h>
 
-//static char launchNotificationKey;
 
 @implementation AppDelegate (notification)
 
-- (id) getCommandInstance:(NSString*)className
-{
-    return [self.viewController getCommandInstance:className];
-}
-
-// its dangerous to override a method from within a category.
-// Instead we will use method swizzling. we set this up in the load call.
-/*+ (void)load
-{
-    Method original, swizzled;
-    
-    original = class_getInstanceMethod(self, @selector(init));
-    swizzled = class_getInstanceMethod(self, @selector(swizzled_init));
-    method_exchangeImplementations(original, swizzled);
-}
-
-- (AppDelegate *)swizzled_init
-{
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(createNotificationChecker:)
-                                                 name:@"UIApplicationDidFinishLaunchingNotification" object:nil];
-    
-    // This actually calls the original init method over in AppDelegate. Equivilent to calling super
-    // on an overrided method, this is not recursive, although it appears that way. neat huh?
-    return [self swizzled_init];
-}*/
-
-// This code will be called immediately after application:didFinishLaunchingWithOptions:. We need
-// to process notifications in cold-start situations
-/*- (void)createNotificationChecker:(NSNotification *)notification
- {
- if (notification)
- {
- NSDictionary *launchOptions = [notification userInfo];
- if (launchOptions)
- self.launchNotification = [launchOptions objectForKey: @"UIApplicationLaunchOptionsRemoteNotificationKey"];
- }
- }
- */
 
 - (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
     NSLog([NSString stringWithFormat:@"Device Token=%@",deviceToken]);
@@ -70,28 +24,6 @@
     }];
 }
 
-/*
- - (void)applicationDidBecomeActive:(UIApplication *)application {
- 
- NSLog(@"active");
- 
- PushPlugin *pushHandler = [self getCommandInstance:@"PushNotification"];
- if (pushHandler.clearBadge) {
- NSLog(@"PushPlugin clearing badge");
- //zero badge
- application.applicationIconBadgeNumber = 0;
- } else {
- NSLog(@"PushPlugin skip clear badge");
- }
- 
- if (self.launchNotification) {
- pushHandler.isInline = NO;
- pushHandler.notificationMessage = self.launchNotification;
- self.launchNotification = nil;
- [pushHandler performSelectorOnMainThread:@selector(notificationReceived) withObject:pushHandler waitUntilDone:NO];
- }
- }
- */
 
 //For interactive notification only
 - (void)application:(UIApplication *)application handleActionWithIdentifier:(NSString *)identifier forRemoteNotification:(NSDictionary *)userInfo completionHandler:(void(^)())completionHandler
@@ -101,24 +33,5 @@
         completionHandler();
     }];
 }
-
-
-// The accessors use an Associative Reference since you can't define a iVar in a category
-// http://developer.apple.com/library/ios/#documentation/cocoa/conceptual/objectivec/Chapters/ocAssociativeReferences.html
-/*- (NSMutableArray *)launchNotification
- {
- return objc_getAssociatedObject(self, &launchNotificationKey);
- }
- 
- - (void)setLaunchNotification:(NSDictionary *)aDictionary
- {
- objc_setAssociatedObject(self, &launchNotificationKey, aDictionary, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
- }
- 
- - (void)dealloc
- {
- self.launchNotification = nil; // clear the association and release the object
- }
- */
 
 @end
